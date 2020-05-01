04cb3d909e54738788b7d870917bdc4234f44f62
saziaup <sazigithub@gmail.com>
2020-05-01 19:41:34 +0000

    part 3 done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,13 +10,10 @@ class MoviesController < ApplicationController
   end
 
   def index
-    if params[:ratings].nil? and params[:commit] 
-        session.delete(:checks)
-    end
     
     @sort = params[:sort] || session[:sort]
     @all_ratings = Movie.all_ratings
-    @checks = params[:ratings] || session[:checks]
+    @checks = params[:ratings] || session[:ratings] || {'G' => '1','PG' => '1','PG-13' => '1','R' => '1'}
 
     session[:checks] = @checks
     session[:sort] = @sort
@@ -26,8 +23,8 @@ class MoviesController < ApplicationController
     else
         @movies = Movie.where(rating: @checks.keys)
     end  
-    @movies = @movies.order(@sort) if @sort   
-  end
+        @movies = @movies.order(@sort) if @sort   
+    end
 
   def new
     # default: render 'new' template
